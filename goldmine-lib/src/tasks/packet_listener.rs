bf7ff3c6506da34f88b8d0fbaaf071ffec968e37
DasBlackfur <64478051+DasBlackfur@users.noreply.github.com>
2024-05-04 11:48:59 +0200

    Switch to declio for packet definition

--- goldmine-lib/src/tasks/packet_listener.rs
+++ goldmine-lib/src/tasks/packet_listener.rs
@@ -1,6 +1,8 @@
 use std::net::SocketAddr;
 
 use anyhow::Result;
+use declio::Decode;
+use declio::Encode;
 use mlua::Function;
 use mlua::LuaSerdeExt;
 use tokio::{net::UdpSocket, sync::watch::Sender};
@@ -8,33 +10,35 @@ use tokio::{net::UdpSocket, sync::watch::Sender};
 use crate::packets::RAKNET_VERSION;
 use crate::{packets::Packet, Server};
 
-pub async fn packet_listener(server: Server, _sender: Sender<Packet>) -> Result<()> {
+pub async fn packet_listener(server: Server, _sender: Sender<String>) -> Result<()> {
     let socket = UdpSocket::bind(server.addr.clone()).await?;
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 1024].to_vec();
 
     loop {
         let (_len, sender_addr) = socket.recv_from(&mut buffer).await?;
         let packet = receive_packet(&buffer, &server)?;
         if let Some(return_packet) = handle_packet(packet, &server, &sender_addr)? {
-            send_packet(&socket, sender_addr, return_packet, &server).await?;
+            send_packet(&mut buffer, &socket, sender_addr, return_packet, &server).await?;
         }
     }
 }
 
-fn receive_packet(buffer: &[u8], server: &Server) -> Result<Packet> {
-    let mut packet = Packet::from_bytes(buffer)?;
+fn receive_packet(mut buffer: &[u8], server: &Server) -> Result<Packet> {
+    let mut packet = Packet::decode((), &mut buffer)?;
     packet = execute_pl_callbacks(packet, server)?;
     Ok(packet)
 }
 
 async fn send_packet(
+    buffer: &mut Vec<u8>,
     socket: &UdpSocket,
     addr: SocketAddr,
     mut packet: Packet,
     server: &Server,
 ) -> Result<()> {
     packet = execute_pl_callbacks(packet, server)?;
-    socket.send_to(&packet.as_bytes()?, addr).await?;
+    packet.encode((), buffer);
+    socket.send_to(buffer, addr).await?;
     Ok(())
 }
 
@@ -53,30 +57,9 @@ fn handle_packet(
     sender_addr: &SocketAddr,
 ) -> Result<Option<Packet>> {
     let return_packet = match packet {
-        Packet::NoOP => None,
-        Packet::CSPingConnections(ping_id) => {
-            let return_packet = Packet::SCPingOpenConnections(
-                ping_id,
-                server.guid,
-                format!("MCCPP;Demo;{}", server.server_name),
-            );
-            Some(return_packet)
+        Packet::CSPingConnections { ping_id, magic } => {
+            None
         }
-        Packet::CSConnectionRequest1(raknet_version, packet_length) => {
-            if raknet_version == RAKNET_VERSION {
-                Some(Packet::SCConnectionReply1(
-                    server.guid,
-                    packet_length as u16,
-                ))
-            } else {
-                Some(Packet::SCIncompatibleProtocol(RAKNET_VERSION, server.guid))
-            }
-        }
-        Packet::CSConnectionRequest2(mtu_size) => Some(Packet::SCConnectionReply2(
-            server.guid,
-            sender_addr.port(),
-            mtu_size,
-        )),
         _ => unreachable!(),
     };
     Ok(return_packet)
